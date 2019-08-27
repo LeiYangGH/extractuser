72f702fb4620507056b68879a100af461d8b573a
LeiYangGH <675686066@qq.com>
2019-08-27 19:58:03 +0800

    include jp nums and output sep files

---
+++ tests/test_tidy_line.py
@@ -0,0 +1,29 @@
+import unittest
+from tidy_user_loc import keep_first_section,keep_key_jpa
+
+class MyTestCase(unittest.TestCase):
+    # def test_something(self):
+    #     self.assertEqual(True, False)
+    # def test_keep_first_section(self):
+    #     line = r'1916Linux	Co. Aontroim/Antrim 脡ire/Ireland 馃嚠馃嚜'
+    #     line = keep_first_section(line)
+    #     print(line)
+    #     self.assertEqual(False, 'Antrim' in line)
+    #     self.assertEqual(False, '/' in line)
+    def test_keep_key_jpa(self):
+        line = r'BUTANiTRO	東京ピッグランド(仮設中)'
+        line = keep_key_jpa(line)
+        print(line)
+        self.assertEqual(False, '仮設中' in line)
+        self.assertEqual(False, 'ピッグランド' in line)
+        self.assertEqual(True, '東京' in line)
+
+    def test_keep_key_jpa_nums(self):
+        line = r'tenmei0	東京都2.1次元'
+        line = keep_key_jpa(line)
+        print(line)
+        self.assertEqual(False, '次元' in line)
+        self.assertEqual(False, '2.1' in line)
+        self.assertEqual(True, '東京' in line)
+if __name__ == '__main__':
+    unittest.main()
--- tidy_user_loc.py
+++ tidy_user_loc.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import re
+import sys
 
 
 def read(filename='huawei_user_loc_check.txt'):
@@ -8,8 +9,8 @@ def read(filename='huawei_user_loc_check.txt'):
         return [line.strip() for line in file.readlines()]
 
 
-def write(lines):
-    with open('output_user_location.txt', "w", encoding='utf-8') as file:
+def write(lines, out_file):
+    with open(out_file, "w", encoding='utf-8') as file:
         for line in lines:
             file.write(f'{line}\r\n')
         file.flush()
@@ -22,8 +23,14 @@ def write(lines):
 reg_locnums_1 = re.compile(r"N\s*(\d+)\s*°\s*(\d+)\s*'\s*(\d+)\s*''\s*[\s\S]*W\s*(\d+)\s*°\s*(\d+)\s*'\s*(\d+)\s*''\s*")
 
 
-def tidy_locnums(line):
+def replace_chars(line):
     line = line.replace(r'ÜT:', '')
+    line = line.replace(r'|', ',')
+    line = line.replace(r'!', ',')
+    return line
+
+
+def tidy_locnums(line):
     match = reg_locnums_1.search(line)
     if match:
         print(line)
@@ -34,8 +41,86 @@ def tidy_locnums(line):
         except Exception as e:
             print('*' * 70)
             print(line)
+    return line
+
+
+reg_comma = re.compile(r"([^/^,\d]{5,})\s*[,，,/][\s\S]*")
+
+
+def keep_first_section(line, notes=False):
+    match = reg_comma.search(line)
+    if match:
+        # print(match.group(1))
+        if notes:
+            sys.stdout.buffer.write(line.encode('utf-8'))
+            print()
+        try:
+            line = reg_comma.sub(r'\1', line)
+            if notes:
+                sys.stdout.buffer.write(line.encode('utf-8'))
+                print()
+                print('-' * 70)
+        except Exception as e:
+            if notes:
+                print('*' * 70)
+                sys.stdout.buffer.write(line.encode('utf-8'))
+    return line
+
+
+reg_ch_jp = re.compile(r'[\u3040-\u30ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff\uff66-\uff9f\(\)\d\.]{5,}')
+
+
+def keep_key_jpa(line, notes=False):
+    match_ch_jp = reg_ch_jp.search(line)
+    if not match_ch_jp:
+        return line
+    matched_ch_jp_text = match_ch_jp.group(0)
+    # if notes:
+        # sys.stdout.buffer.write(matched_ch_jp_text)
+        # print(matched_ch_jp_text)
+    # loopcnt = 0
+
+    for key in ['东京', '東京', '大阪', '横滨', '名古屋', '神户', '福冈', '京都', '札幌', '仙台', '广岛']:
+        if key in matched_ch_jp_text:
+            try:
+                print(line)
+                # loopcnt += 1
+                # print(loopcnt)
+                if notes:
+                    # sys.stdout.buffer.write(line.encode('utf-8'))
+                    print(line)
+                    print()
+                line = line.replace(matched_ch_jp_text, key)
+                if notes:
+                    # sys.stdout.buffer.write(line.encode('utf-8'))
+                    print(line)
+                    print()
+                    print('-' * 70)
+            except:
+                pass
+            break
+
+    return line
+
+
+
 
 if __name__ == "__main__":
     alllines = read()
     print(len(alllines))
-    [tidy_locnums(line) for line in alllines]
+    alllines = [replace_chars(line) for line in alllines]
+
+    loc_lines = [line for line in alllines if reg_locnums_1.search(line)]
+    non_loc_lines = [line for line in alllines if not reg_locnums_1.search(line)]
+
+    loc_lines = [tidy_locnums(line) for line in loc_lines]
+
+    non_loc_lines = [keep_first_section(line, notes=True) for line in non_loc_lines]
+    non_loc_lines = [keep_key_jpa(line, notes=True) for line in non_loc_lines]
+
+
+    print(len(loc_lines))
+    print(len(non_loc_lines))
+    write(loc_lines,"output_loc_lines.txt")
+    write(non_loc_lines,"output_non_loc_lines.txt")
+    print('all done.')
